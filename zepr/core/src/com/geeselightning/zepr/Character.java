d588d3326d12d20f2a72aa8119d271a0a56886f8
JedFenn <jed.fenn@gmail.com>
2018-11-26 14:46:33 +0000

    updated character to not refer to player or zombie

--- zepr/core/src/com/geeselightning/zepr/Character.java
+++ zepr/core/src/com/geeselightning/zepr/Character.java
@@ -20,28 +20,28 @@ public class Character extends Sprite {
 
     @Override
     public void draw(Batch batch) {
-        // Calls the update method of the player. To update its properties, i.e. position.
-        // The method is given the parameter delta so it can calculate the players new position.
+        // Calls the update method of the character. To update its properties, i.e. position.
+        // The method is given the parameter delta so it can calculate the character new position.
         update(Gdx.graphics.getDeltaTime());
 
         super.draw(batch);
     }
 
     /**
-     * Finds the direction (in degrees) that an object is in relative to the player.
+     * Finds the direction (in degrees) that an object is in relative to the character.
      * @param coordinate 2d vector representing the position of the object
-     * @return bearing   value in radians of the bearing from the player to the coordinate
+     * @return bearing   value in radians of the bearing from the character to the coordinate
      */
     protected double getDirection(Vector2 coordinate) {
-        Vector2 playerCenter = new Vector2(this.getX() + (getWidth()/ 2),
+        Vector2 charCenter = new Vector2(this.getX() + (getWidth()/ 2),
                 this.getY()+ (getHeight() / 2));
 
-        // atan2 is uses the signs of both variables the determine the correct quadrant (relative to the player) of the
+        // atan2 is uses the signs of both variables the determine the correct quadrant (relative to the character) of the
         // result.
         // Modulus 2pi of the angle must be taken as the angle is negative for the -x quadrants.
         // The angle must first be displaced by 2pi because the Java modulus function can return a -ve value.
 
-        return(Math.atan2((coordinate.x - playerCenter.x), (coordinate.y - playerCenter.y)) + (2 * Math.PI))
+        return(Math.atan2((coordinate.x - charCenter.x), (coordinate.y - charCenter.y)) + (2 * Math.PI))
                 % (2 * Math.PI);
     }
 
@@ -51,20 +51,20 @@ public class Character extends Sprite {
      * @return normalised 2d vector that from this will point towards given coordinate
      */
     protected Vector2 getDirNormVector(Vector2 coordinate) {
-        Vector2 zombieCenter = new Vector2(this.getX() + (getWidth() / 2),
+        Vector2 charCenter = new Vector2(this.getX() + (getWidth() / 2),
                 this.getY() + (getHeight() / 2));
-
-        Vector2 diffVector = new Vector2((coordinate.x - zombieCenter.x), (coordinate.y - zombieCenter.y));
+        // create vector that is the difference between character and the coordinate, and return it normalised
+        Vector2 diffVector = new Vector2((coordinate.x - charCenter.x), (coordinate.y - charCenter.y));
         return diffVector.nor();
     }
 
     /**
-     * This method updates all the players properties.
+     * This method updates all the characters properties.
      *
      * @param delta the time passed since the last frame (render() call)
      */
     public void update(float delta) {
-        // Update x, y position of player.
+        // Update x, y position of character.
         // New position is the old position plus the distance moved as a result of the velocity
         setX(getX()  + velocity.x * delta);
         setY(getY() + velocity.y * delta);
