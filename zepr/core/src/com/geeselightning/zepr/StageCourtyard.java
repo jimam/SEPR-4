6f2ec3f18dd816b93d43ae3c8629877cdd1844f6
Tom Elvidge <tee504@york.ac.uk>
2018-12-04 13:10:36 +0000

    Added new stages

--- zepr/core/src/com/geeselightning/zepr/StageCourtyard.java
+++ zepr/core/src/com/geeselightning/zepr/StageCourtyard.java
@@ -3,13 +3,13 @@ package com.geeselightning.zepr;
 import com.badlogic.gdx.maps.tiled.TiledMap;
 import com.badlogic.gdx.math.Vector2;
 
-public class StageTown extends Stage {
+public class StageCourtyard extends Stage {
 
-    private static final String mapLocation = "core/assets/maps/town.tmx";
+    private static final String mapLocation = "core/assets/maps/courtyard.tmx";
     private static final Vector2 playerSpawn = new Vector2(0, 0);
     private static final Vector2 testZombieSpawn = new Vector2(0, 50);
 
-    public StageTown(Zepr zepr) {
+    public StageCourtyard(Zepr zepr) {
         super(zepr, mapLocation, playerSpawn, testZombieSpawn);
     }
 }
