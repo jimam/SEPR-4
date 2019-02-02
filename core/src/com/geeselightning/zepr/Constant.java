7875cbb4d43ece3d2c57c7f24da6be662c29bcde
Xzytl <5693695+Xzytl@users.noreply.github.com>
2019-02-02 21:09:12 +0000

    Update Constant.java
    Fixing formatting

--- core/src/com/geeselightning/zepr/Constant.java
+++ core/src/com/geeselightning/zepr/Constant.java
@@ -2,44 +2,48 @@ package com.geeselightning.zepr;
 
 import com.badlogic.gdx.math.Vector2;
 
-    public final class Constant {
-    public static final Vector2 ORIGIN = new Vector2(0, 0);
-    public static final float PLAYERSPEED = 120;
-    public static final int PLAYERMAXHP = 100;
-    public static final int PLAYERDMG = 20;
-    public static final int PLAYERRANGE = 50;
-    public static final float PLAYERHITCOOLDOWN = 0.2f;
-    
-    public static final float ZOMBIESPEED = 80;
-    public static final int ZOMBIEMAXHP = 100;
-    public static final int ZOMBIEDMG = 10;
-    public static final int ZOMBIERANGE = 20;
-    public static final float ZOMBIEHITCOOLDOWN = 1;
-    
-    public static final float SLOWSPEEDMULT = 0.5f;
-    public static final float SLOWHPMULT = 2f;
-    public static final float SLOWDMGMULT = 1;
-    
-    public static final float MEDSPEEDMULT = 1;
-    public static final float MEDHPMULT = 1.5f;
-    public static final float MEDDMGMULT = 2f;
-    
-    public static final float FASTSPEEDMULT = 2f;
-    public static final float FASTHPMULT = 1;
-    public static final float FASTDMGMULT = 1;
-    public static final float NERDYHPMULT = 1.5f;
-    public static final float NERDYDMGMULT = 1;
-    public static final float NERDYSPEEDMULT = 1;
-    public static final float SPORTYHPMULT = 1;
-    public static final float SPORTYDMGMULT = 1;
-    public static final float SPORTYSPEEDMULT = 1.5f;
-    public static final float HEAVYDMGMULT = 1.5f;
-	  public static final float HEAVYHPMULT = 1;
-	  public static final float HEAVYSPEEDMULT = 1;
+public final class Constant {
 	
-    public static final int HEALUP = 30;
-    public static final int SPEEDUP = 50;
-    public static final float SPEEDUPTIME = 10;
-    public static final float IMMUNITYTIME = 5;
-    public static final float ATKUPTIME = 10;
+	public static final int PPM = 50;
+	
+	public static final Vector2 ORIGIN = new Vector2(0, 0);
+	public static final float PLAYERSPEED = 120;
+	public static final int PLAYERMAXHP = 100;
+	public static final int PLAYERDMG = 20;
+	public static final int PLAYERRANGE = 50;
+	public static final float PLAYERHITCOOLDOWN = 0.2f;
+
+	public static final float ZOMBIESPEED = 80;
+	public static final int ZOMBIEMAXHP = 100;
+	public static final int ZOMBIEDMG = 10;
+	public static final int ZOMBIERANGE = 20;
+	public static final float ZOMBIEHITCOOLDOWN = 1;
+
+	public static final float SLOWSPEEDMULT = 0.5f;
+	public static final float SLOWHPMULT = 2f;
+	public static final float SLOWDMGMULT = 1;
+
+	public static final float MEDSPEEDMULT = 1;
+	public static final float MEDHPMULT = 1.5f;
+	public static final float MEDDMGMULT = 2f;
+
+	public static final float FASTSPEEDMULT = 2f;
+	public static final float FASTHPMULT = 1;
+	public static final float FASTDMGMULT = 1;
+	public static final float NERDYHPMULT = 1.5f;
+	public static final float NERDYDMGMULT = 1;
+	public static final float NERDYSPEEDMULT = 1;
+	public static final float SPORTYHPMULT = 1;
+	public static final float SPORTYDMGMULT = 1;
+	public static final float SPORTYSPEEDMULT = 1.5f;
+	public static final float HEAVYDMGMULT = 1.5f;
+	public static final float HEAVYHPMULT = 1;
+	public static final float HEAVYSPEEDMULT = 1;
+
+	public static final int HEALUP = 30;
+	public static final int SPEEDUP = 50;
+	public static final float SPEEDUPTIME = 10;
+	public static final float IMMUNITYTIME = 5;
+	public static final float ATKUPTIME = 10;
+	
 }
