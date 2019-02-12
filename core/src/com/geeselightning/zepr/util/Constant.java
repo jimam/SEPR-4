83ed02d7450ab3ca1c10af8ff91c3ab14e7375e5
Xzytl <5693695+Xzytl@users.noreply.github.com>
2019-02-12 20:39:17 +0000

    Massively reduced zombie and player movement speed. May require further changes.

--- core/src/com/geeselightning/zepr/util/Constant.java
+++ core/src/com/geeselightning/zepr/util/Constant.java
@@ -8,13 +8,13 @@ public final class Constant {
 	public static final int PPT = 32;
 	
 	public static final Vector2 ORIGIN = new Vector2(0, 0);
-	public static final float PLAYERSPEED = 120;
+	public static final float PLAYERSPEED = 10;
 	public static final int PLAYERMAXHP = 100;
 	public static final int PLAYERDMG = 20;
 	public static final int PLAYERRANGE = 50;
 	public static final float PLAYERHITCOOLDOWN = 0.2f;
 
-	public static final float ZOMBIESPEED = 80;
+	public static final float ZOMBIESPEED = 5;
 	public static final int ZOMBIEMAXHP = 100;
 	public static final int ZOMBIEDMG = 10;
 	public static final int ZOMBIERANGE = 20;
