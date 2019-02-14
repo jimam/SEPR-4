b4386d80744247a5d952bcf60c3800cf7b63c9bb
Xzytl <5693695+Xzytl@users.noreply.github.com>
2019-02-14 10:17:28 +0000

    Added basic boss implementation

--- core/src/com/geeselightning/zepr/entities/BossZombie.java
+++ core/src/com/geeselightning/zepr/entities/BossZombie.java
@@ -6,9 +6,14 @@ import com.geeselightning.zepr.game.Zepr;
 import com.geeselightning.zepr.util.Constant;
 
 public class BossZombie extends Zombie {
+	
+	public enum Type {
+		MINIBOSS,
+		BOSS
+	}
 
 	public BossZombie(Zepr parent, Sprite sprite, float bRadius, Vector2 initialPos, float initialRot, Type type) {
-		super(parent, sprite, bRadius, initialPos, initialRot, type);
+		super(parent, sprite, bRadius, initialPos, initialRot, null);
 		this.speed = (int) (Constant.ZOMBIESPEED * 0.75f);
 		this.health = (int) (Constant.ZOMBIEMAXHP * 5f);
 		this.attackDamage = (int) (Constant.ZOMBIEDMG * 2f);
