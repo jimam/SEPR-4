56f589f2d4e8bfe45fcdedc6cd6ea7485782d611
Kevin O'Donnell <kaod500@york.ac.uk>
2019-03-14 11:23:31 +0000

    fixed cure mechanics

--- core/src/com/geeselightning/zepr/game/GameManager.java
+++ core/src/com/geeselightning/zepr/game/GameManager.java
@@ -264,8 +264,10 @@ public class GameManager implements Disposable {
 		List<Zombie> aliveZombies = zombies.stream().filter(e -> (e.isAlive()))
 				.collect(Collectors.toList());
 		aliveZombies.forEach(e -> {
-			e.setType(Zombie.Type.HUMAN);
-			e.setSprite("player01.png");
+		    if(player.closeEnough(e)){
+                e.setType(Zombie.Type.HUMAN);
+                e.setSprite("player01.png");
+            }
 		});
 	}
 
@@ -557,9 +559,10 @@ public class GameManager implements Disposable {
 		speed *= modifier;
 
 		if (controller.F) {
-			if(cureCooldown == 0){
+		    System.out.println(cureCooldown);
+			if(cureCooldown <= 0){
 				cureZombies();
-				cureCooldown = 3;
+				cureCooldown = 3f;
 			}
 		}
 		if (controller.left) {
